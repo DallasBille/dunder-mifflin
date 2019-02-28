2731a7f47c1824dd24cd2cfea177f0108a7170a4
github username <github email address>
2019-02-28 16:34:29 -0500

    progress so far

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+
+    before_action :get_dog, only: [:show]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @employees = @dog.employees
+    end
+
+    private
+    def get_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit(:breed, :name, :age)
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,43 @@
 class EmployeesController < ApplicationController
+    before_action :get_employee, only: [:show, :edit, :update]
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+
+    end
+    def new
+        @employee = Employee.new
+        @dogs = Dog.all
+    end
+    def create
+        @employee = Employee.create(employee_params)
+        redirect_to  @employee
+    end
+
+    def edit
+        @dogs = Dog.all
+    end
+
+    def update
+        @employee = Employee.new(employee_params)
+
+        if @employee.valid?
+            @employee.update
+            redirect_to @employee
+        else
+            render :edit
+        end
+    end
+
+    private
+    def get_employee
+        @employee = Employee.find(params[:id])
+    end
+
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url, :dog_id)
+    end
 end
